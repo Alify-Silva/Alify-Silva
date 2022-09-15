89cb16de704f3e5501b20896a29151baf8a11107
Alify Silva <alify.silva@outlook.com>
2022-09-15 14:52:24 -0300

    Alterations

--- css-avancado/curso-dev-web/aula-1/js/javascript.js
+++ css-avancado/curso-dev-web/aula-1/js/javascript.js
@@ -1,1 +1,10 @@
-document.write('<h3> Olá mano!</h3>')
+var nota = parseInt(prompt('Digite a nota 1 do aluno'))
+var faltas = parseInt(prompt('Digite a quantidade de faltas do aluno'))
+
+/**/
+
+if (nota >= 7 && faltas <= 7) {
+    document.write("<span id='container-nav-aprovado' >Aprovado!</span>")
+} else if ((nota <= 7 && faltas >7) || (nota>=7 && faltas >7)) {
+    document.write("<span id='container-nav-reprovado' >Reprovado!</span>")
+}
