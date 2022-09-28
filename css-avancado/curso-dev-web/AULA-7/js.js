b66fd224e8377ba778ec5653c79fda675702a306
Alify Silva <alify.silva@outlook.com>
2022-09-28 15:17:53 -0300

    Gyhuji@13

---
+++ css-avancado/curso-dev-web/AULA-7/js.js
@@ -0,0 +1,25 @@
+// VARIAVEIS
+var nome = 'Alify Formidable';
+
+// PROPRIEDADE length
+document.write(nome.length); // Retorna o tamanho de caracteres na String
+document.write('<br>');
+document.write(nome.charAt(2)); // Retorna o valor armazenado na posição da String
+document.write('<br>');
+document.write(nome.indexOf('g')); //Retorna a posição da letra da String
+document.write('<br>');
+document.write(nome.replace('Sant Ana', 'Silva')); // Substituição de valores na String
+document.write('<br>');
+document.write(nome.substring(4,10)); // Retorna String com valores entre uma coluna de String denominada com começo e fim
+document.write('<br>');
+document.write(nome.toLowerCase()); // Deixa toda String LOW CASE - MINÚSCULAS
+document.write('<br>');
+document.write(nome.toUpperCase()); // Deixa toda String UPPER CASE - MAÍUSCULAS
+document.write('<br>');
+document.write(nome.trim()); // Remove os espaços em branco da String
+document.write('<br>');
+document.write(nome.valueOf());
+document.write('<br>');
+document.write('<br>');
+document.write('<hr>');
+document.wirte('<h2> Funções - Matemáticas </h2>');
--- css-avancado/curso-dev-web/aula-6/js.js
+++ css-avancado/curso-dev-web/aula-6/js.js
@@ -1,23 +1,41 @@
 
+/* 
+*
+CRIAÇÃO DAS FUNÇÕES 
+*
+*/
 
 function exibirArtigo(id, callbackSucesso, callbackErro){
     //Finge que tem uma lógica sobre: recuperar o id via requisição HTTP
-    if (true){
+    if (false){
         callbackSucesso('Funções de callback em JS ', 'Funções de callback são muito utilizadas');
     } else {
         callbackErro('Erro ao recuperar os dados solicitados');
     }
-
-    varcallbackSucesso = function(){
-
-    }
-
-    exibirArtigo(1, callbackSucesso(), callbackErro());
-
 }
 
-try {
-    
-} catch {
-    
+/*
+*
+CRIAÇÃO DAS VARIÁVEIS
+*
+*/
+
+var callbackSucesso = function(titulo, descricao){
+    document.write('<h1>'+titulo+'</h1>')
+    document.write('<hr />')
+    document.write('<p>'+descricao+'</p>')
 }
+
+var callbackErro = function(erro){
+    document.write('<p><b>Erro: </b>'+erro+'</p>');
+}
+
+/**
+ * 
+ * CORPO DO PROGRAMA
+ * 
+ */
+// VALOR, SE DER CERTO FAÇA, SE DER ERRADO, FAÇA
+exibirArtigo(1, this.callbackSucesso, this.callbackErro);
+
+
